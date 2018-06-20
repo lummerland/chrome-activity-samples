d19eaac72d2b01c9acf4433ddb735cb0781b3b2b
Manuel Kummerländer <mkummerlaender@seibert-media.net>
2018-06-20 14:16:10 +0200

    Added ability to close popup

--- dialog.js
+++ dialog.js
@@ -4,4 +4,12 @@ document.addEventListener('DOMContentLoaded', function() {
 	console.log("loaded dialog");
 	var now = new Date();
 	document.getElementById('time').textContent = now.toTimeString();
+	setTimeout(window.close, 5000);
 });
+
+document.onkeydown = function(evt) {
+    evt = evt || window.event;
+    if (evt.keyCode == 27) {
+        window.close();
+    }
+};
