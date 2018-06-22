39d3bcfd76d59f0698e334598e5abbcc36d7eb43
Manuel Kummerländer <mkummerlaender@seibert-media.net>
2018-06-22 06:03:01 +0200

    Extracted settings

--- src/js/settings.js
+++ src/js/settings.js
@@ -2,24 +2,16 @@
 
 document.addEventListener('DOMContentLoaded', function() {
 
-	chrome.storage.sync.get(['frequency'], function(data) {
-		if (data['frequency'] != null) {
-     		console.log('Settings retrieved', data);
-     		document.getElementById('frequency').value = data['frequency'];
-		}
+	Setting.getFrequency( function(frequency) {
+		document.getElementById('frequency').value = frequency;
     });
 
 	var saveAction = document.getElementById('save');
 	
 	saveAction.onclick = function(element) {
-		var frequency = document.getElementById('frequency').value;
-		if (!frequency) {
-			console.log("No frequency to save!");
-			return;
-		};
-		chrome.storage.sync.set({'frequency': frequency}, function() {
-	     	console.log('Settings saved');
-		});
+		Setting.setFrequency(
+			document.getElementById('frequency').value
+		);
 	}
 
 });
